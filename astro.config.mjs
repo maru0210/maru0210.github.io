595e70f4d084a3f38f4fcb0beca678b48b7391de
maru0210 <m.ryuto0210@gmail.com>
2023-12-12 20:16:25 +0900

    Add Tailwind CSS configuration and dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://maru0210.github.io'
+  site: 'https://maru0210.github.io',
+  integrations: [tailwind()]
 });
